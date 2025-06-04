618cb81c6e9b75447346357c144bc521c6466ea9
Rothsource <rsovannroth@gmail.com>
2025-06-04 21:00:13 +0700

    Product

---
+++ Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    String name;
+    String category;
+    double price;
+    int stock;
+
+    public Product(String name, String category, double price, int stock) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+    }
+}
