98194b6b30f0bad864e81b0bc03198e1cf3a5ead
Rothsource <rsovannroth@gmail.com>
2025-06-04 22:19:30 +0700

    Update Order

--- Order.java
+++ Order.java
@@ -1,17 +1,23 @@
-import java.util.Date;
-
+public import java.util.Date;
 public class Order {
     int orderId;
     Customer customer;
-    double amount;
+    double quantity;
     String status; // e.g., "Pending", "Shipped"
     Date orderTime;
+    int total_price;
+    int employee_id;
+    String product_review;
+    int product_id;
 
-    public Order(int orderId, Customer customer, double amount, String status, Date orderTime) {
+
+    public Order(int orderId, Customer customer, double quantity, String status, Date orderTime,int employee_id,int total_price) {
         this.orderId = orderId;
         this.customer = customer;
-        this.amount = amount;
+        this.quantity = quantity;
         this.status = status;
         this.orderTime = orderTime;
+        this.employee_id = employee_id;
+        this.total_price = total_price
     }
 }
