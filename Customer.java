8731b5b7012eb53532682d6d7d30772b8394f550
Rothsource <rsovannroth@gmail.com>
2025-06-04 21:59:35 +0700

    Update Customer

--- Customer.java
+++ Customer.java
@@ -3,11 +3,20 @@ public class Customer {
     char gender; // M or F
     String phoneNumber;
     String address;
+    String registerDATE;
+    String dob;
+    String password;
+    int loyalty_points;
 
-    public Customer(String userName, char gender, String phoneNumber, String address) {
+    public Customer(String userName, char gender, String phoneNumber, String address, String registerDATE, String dob, String password,int loyalty_points) {
         this.userName = userName;
         this.gender = gender;
         this.phoneNumber = phoneNumber;
         this.address = address;
+        this.registerDATE = registerDATE;
+        this.dob = dob;
+        this.password = password;
+        this.loyalty_points = loyalty_points;
+
     }
 }
