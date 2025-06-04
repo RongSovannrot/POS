9b6700cd8f6b015a044518f4afb559af85088dcd
Rothsource <rsovannroth@gmail.com>
2025-06-04 20:49:35 +0700

    Customer

---
+++ Customer.java
@@ -0,0 +1,13 @@
+public class Customer {
+    String userName;
+    char gender; // M or F
+    String phoneNumber;
+    String address;
+
+    public Customer(String userName, char gender, String phoneNumber, String address) {
+        this.userName = userName;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+}
