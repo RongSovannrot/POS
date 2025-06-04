4633f70b2981e02993960b34a56dd453c79c0b21
Rothsource <rsovannroth@gmail.com>
2025-06-04 22:21:01 +0700

    Update Prodcut

--- Product.java
+++ Product.java
@@ -3,11 +3,15 @@ public class Product {
     String category;
     double price;
     int stock;
-
-    public Product(String name, String category, double price, int stock) {
+    int product_id;
+    int promotion;
+    int employee_id;
+    char size; 
+// Constructor TO ADD product to order
+    public Product(String name, String category, double price, char size) {
         this.name = name;
         this.category = category;
         this.price = price;
-        this.stock = stock;
+        this.size = size;
     }
 }
