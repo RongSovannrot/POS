ab614b8543e38c2c9cbc3e4f742ca2196622a47d
Rothsource <rsovannroth@gmail.com>
2025-06-04 21:00:53 +0700

    Staff

---
+++ Staff.java
@@ -0,0 +1,11 @@
+public class Staff {
+    String staffName;
+    String staffId;
+    String role; // e.g., "Cashier", "Manager"
+
+    public Staff(String staffName, String staffId, String role) {
+        this.staffName = staffName;
+        this.staffId = staffId;
+        this.role = role;
+    }   
+}
