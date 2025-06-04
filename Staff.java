2cbff1034e6ea1f37eaa83972ed3599374f6c5b9
Rothsource <rsovannroth@gmail.com>
2025-06-04 22:21:43 +0700

    Update Staff

--- Staff.java
+++ Staff.java
@@ -2,10 +2,17 @@ public class Staff {
     String staffName;
     String staffId;
     String role; // e.g., "Cashier", "Manager"
+    String shift;
+    String Phone_number;
+    double salary;
+    String address;
+    String IDnumber;
 
-    public Staff(String staffName, String staffId, String role) {
+    public Staff(String staffName, String staffId, String role, String IDnumber;) {
         this.staffName = staffName;
         this.staffId = staffId;
         this.role = role;
+        this.IDnumber = IDnumber;
+        
     }   
 }
