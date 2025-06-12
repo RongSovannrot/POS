f8572a5ba7ea262d4c7505d7cc9ceb48ec02fae4
Rothsource <rsovannroth@gmail.com>
2025-06-12 20:27:05 +0700

    Update Product

--- Product.java
+++ Product.java
@@ -1,17 +1,82 @@
 public class Product {
-    String name;
-    String category;
-    double price;
-    int stock;
-    int product_id;
-    int promotion;
-    int employee_id;
-    char size; 
-// Constructor TO ADD product to order
+    private String name;
+    private String category;
+    private double price;
+    private int stock;
+    private int product_id;
+    private int promotion;
+    private int employee_id;
+    private char size;
+
+    //Cons
     public Product(String name, String category, double price, char size) {
         this.name = name;
         this.category = category;
         this.price = price;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getProductId() {
+        return product_id;
+    }
+
+    public void setProductId(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public int getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(int promotion) {
+        this.promotion = promotion;
+    }
+
+    public int getEmployeeId() {
+        return employee_id;
+    }
+
+    public void setEmployeeId(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setSize(char size) {
         this.size = size;
     }
 }
