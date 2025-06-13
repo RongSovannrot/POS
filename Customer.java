53ede55762fc9940be21250e6a85bcafaf008efa
Rothsource <rsovannroth@gmail.com>
2025-06-13 21:27:47 +0700

    Update Customer

--- Customer.java
+++ Customer.java
@@ -1,24 +1,66 @@
 public class Customer {
-    String userName;
-    char gender; // M or F
-    String phoneNumber;
-    String address;
-    String registerDATE;
-    String dob;
-    String password;
-    String email;
-    int loyalty_points;
+    private String userName;
+    private char gender; // M or F
+    private String phoneNumber;
+    private String address;
+    private String registerDate;
+    private String dob;
+    private int loyaltyPoints;
+    private String password;
 
-    public Customer(String userName, char gender, String phoneNumber, String address, String registerDATE, String password,) {
+    //cons
+    public Customer(String userName, char gender, String phoneNumber, String address, String registerDate, String dob, String password) {
         this.userName = userName;
         this.gender = gender;
         this.phoneNumber = phoneNumber;
         this.address = address;
-        this.registerDATE = registerDATE;
+        this.registerDate = registerDate;
+        this.dob = dob;
         this.password = password;
-   
+        this.loyaltyPoints = 0;
+    }
 
-    
+    public String getUserName() {
+        return userName;
+    }
 
+    public char getGender() {
+        return gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getRegisterDate() {
+        return registerDate;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public int getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addPoints(int points) {
+        this.loyaltyPoints += points;
     }
 }
