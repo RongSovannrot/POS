2823ce894c7e5eb3e78efde8dfe16c38328a9e85
Rothsource <rsovannroth@gmail.com>
2025-06-13 22:06:22 +0700

    Update

--- Order.java
+++ Order.java
@@ -3,7 +3,6 @@ import java.util.Date;
 public class Order {
     private int orderId;
     private Customer customer;
-<<<<<<< HEAD
     private Product product;
     private int quantity;
     private String status; // Pending, Completed
@@ -13,36 +12,15 @@ public class Order {
 
     //cons
     public Order(int orderId, Customer customer, Product product, int quantity, String status, int employeeId) {
-=======
-    private double quantity;
-    private String status; // e.g., "Pending", "Shipped"
-    private Date orderTime;
-    private int totalPrice;
-    private int employeeId;
-    private String productReview;
-    private int productId;
-
-    // Constructor
-    public Order(int orderId, Customer customer, double quantity, String status, Date orderTime, int employeeId, int totalPrice) {
->>>>>>> 75bf279625213463b28136f945920f146e23d249
         this.orderId = orderId;
         this.customer = customer;
         this.product = product;
         this.quantity = quantity;
         this.status = status;
-<<<<<<< HEAD
         this.orderTime = new Date();
         this.employeeId = employeeId;
     }
 
-=======
-        this.orderTime = orderTime;
-        this.employeeId = employeeId;
-        this.totalPrice = totalPrice;
-    }
-
-    // Getters and setters for access control
->>>>>>> 75bf279625213463b28136f945920f146e23d249
     public int getOrderId() {
         return orderId;
     }
@@ -51,15 +29,11 @@ public class Order {
         return customer;
     }
 
-<<<<<<< HEAD
     public Product getProduct() {
         return product;
     }
 
     public int getQuantity() {
-=======
-    public double getQuantity() {
->>>>>>> 75bf279625213463b28136f945920f146e23d249
         return quantity;
     }
 
@@ -71,13 +45,6 @@ public class Order {
         return orderTime;
     }
 
-<<<<<<< HEAD
-=======
-    public int getTotalPrice() {
-        return totalPrice;
-    }
-
->>>>>>> 75bf279625213463b28136f945920f146e23d249
     public int getEmployeeId() {
         return employeeId;
     }
@@ -86,26 +53,11 @@ public class Order {
         return productReview;
     }
 
-<<<<<<< HEAD
-=======
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductReview(String productReview) {
-        this.productReview = productReview;
-    }
-
->>>>>>> 75bf279625213463b28136f945920f146e23d249
     public void setStatus(String status) {
         this.status = status;
     }
 
-<<<<<<< HEAD
     public void setProductReview(String review) {
         this.productReview = review;
     }
-=======
-    // Other setters can be added as needed, depending on business logic
->>>>>>> 75bf279625213463b28136f945920f146e23d249
 }
